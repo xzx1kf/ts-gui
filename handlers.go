72e42b4349e34873dc04f5a474f5b2df20a12796
Nick Hale <nicholas.hale@gmail.com>
2018-06-26 16:32:15 +0100

    Initial commit

---
+++ handlers.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"html/template"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	var b Bookings
+
+	res, err := http.Get("http://127.0.0.1:8080/bookings")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code errorL %d %s",
+			res.StatusCode,
+			res.Status)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	json.Unmarshal([]byte(body), &b)
+
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Print("template parsing errorL ", err)
+	}
+	err = t.Execute(w, b[0])
+	if err != nil {
+		log.Print("template executing error: ", err)
+	}
+	//fmt.Println(b[0].Court)
+}
